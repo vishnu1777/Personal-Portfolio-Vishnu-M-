8f60c6feadd9ca0b58f10557fa495e3487abfed1
Vishnu <vappu5429@gmail.com>
2023-04-25 22:57:25 +0530

    Got back to index.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: false,
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
---
+++ pages/index.js
@@ -0,0 +1,40 @@
+import "../styles/globals.css";
+import {
+  BottomNavigator,
+  Footer,
+  Navbar,
+  About,
+  Hero,
+  GetStarted,
+  Explore,
+  Insights,
+  Feedback,
+} from "../components";
+
+const Page = () => (
+  <div className="bg-primary-black overflow-hidden">
+    <Navbar />
+    <Hero />
+
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+      <Explore />
+      <BottomNavigator />
+    </div>
+    <div className="relative">
+      <GetStarted />
+      <BottomNavigator />
+    </div>
+    <div className="relative">
+      <Insights />
+
+      <div className="gradient-04 z-0" />
+      <Feedback />
+      <BottomNavigator />
+    </div>
+    <Footer />
+  </div>
+);
+
+export default Page;
