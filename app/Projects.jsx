c90fbbcd8715ae0c22fe71032cefc830c24d5dbe
Vishnu <vappu5429@gmail.com>
2023-04-28 13:31:00 +0530

    Nextjs official bug for ISR: works only on dev server not in production

--- app/Projects.jsx
+++ app/Projects.jsx
@@ -2,6 +2,7 @@ import { client } from "../lib/client";
 import { groq } from "next-sanity";
 import { Works } from "../components";
 export const revalidate = 60;
+
 async function Projects() {
   const query = groq`*[_type == "works"]`;
 
